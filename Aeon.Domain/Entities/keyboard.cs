34825238f8e0e6d4aa04f9776a0e7aab65befcd7
Caio P <caiopontes1707@hotmail.com>
2025-04-21 23:23:22 -0300

    fix(domain): prevent error when GUID is not provided in constructor
    Ensure a default GUID is generated when none is passed to the constructor.
    Só mais esse commit :(

--- Aeon.Domain/Entities/keyboard.cs
+++ Aeon.Domain/Entities/keyboard.cs
@@ -2,7 +2,7 @@
 
 public class Keyboard
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public int RowCount { get; set; }
